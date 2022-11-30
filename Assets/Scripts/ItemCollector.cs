e9cf1a52f7514ea54b0de1b95a5a7a0eea9852d9
Branden Bluhm <bbluhm@my.centennialcollege.ca>
2022-11-30 16:55:01 -0500

    Added Game music, Added arrow collectibles, fixed jump height

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -6,8 +6,10 @@ public class ItemCollector : MonoBehaviour
 {
 
     private int coins = 0;
+    private int arrows = 5;
 
     [SerializeField] private Text coinsText;
+    [SerializeField] private Text arrowsText;
  private void OnTriggerEnter2D(Collider2D collision)
 {
     if (collision.gameObject.CompareTag("Coin"))
@@ -16,6 +18,11 @@ public class ItemCollector : MonoBehaviour
         coins++;
         coinsText.text = "Coins: " + coins;
     }
+        if (collision.gameObject.CompareTag("Arrow"))
+    {
+        Destroy(collision.gameObject);
+        arrows = arrows + 5;
+        arrowsText.text = "Arrows: " + arrows;
+    }
 }
-
 }
